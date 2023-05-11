0406bc94dcde752d89696e1b154d124b5fdf3a33
Danilo Francisquete <126702032+DaniloFrancisquete@users.noreply.github.com>
2023-05-11 11:20:55 -0300

    iniciando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponet } from './meu-primeiro/meu-primeiro.componet';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponet,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
