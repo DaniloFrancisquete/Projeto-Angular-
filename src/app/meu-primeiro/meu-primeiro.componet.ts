6d267509d173b34a16ce23e76a1f373613069dd1
Danilo Francisquete <126702032+DaniloFrancisquete@users.noreply.github.com>
2023-05-11 11:27:35 -0300

    refatorando projeto

--- src/app/meu-primeiro/meu-primeiro.componet.ts
+++ src/app/meu-primeiro/meu-primeiro.componet.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector:'meu-primeiro-componet',
     template:`
-    <p>Meu primeiro componet com Angular 2!</p>
+    <p>Meu primeiro componet com Angular 2</p>
     `
 })
  export class MeuPrimeiroComponet{ }
